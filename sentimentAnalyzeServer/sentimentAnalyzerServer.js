17133f75a49cb1fd51d4b9a98a21c2127931069a
sgeletta <sgeletta@dmu.edu>
2021-03-11 00:05:31 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -12,7 +12,7 @@ function getNLUInstance(){
     const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
         version: '2020-08-01',
         authenticator: new IamAuthenticator({
-            api_key: api_key,
+            apikey: api_key,
         }),
         serviceURL: api_url,
     });
